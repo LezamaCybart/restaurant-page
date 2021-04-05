c2090e82353fad1e7d6645c057c87e745f466082
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-04-05 18:19:28 -0300

    implemented tab-switching logic inside index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ import { populateMenu } from "./menu";
 import { initializeDomElements } from "./template";
 
 window.onload = () => {
-    initializeDomElements(populateContact);
+
+    initializeDomElements(populateHome);
+
+    // tab logic
+    let tabs = document.getElementsByClassName("button");
+    tabs = Array.from(tabs);
+    tabs.forEach(tab => {
+        tab.addEventListener("click", goToTab);
+    });
 }
+
+const goToTab = (event) => {
+    let tabName = event.target.textContent;
+    let contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "";
+
+    if (tabName === "Home") initializeDomElements(populateHome);
+    if (tabName === "Menu") initializeDomElements(populateMenu);
+    if (tabName === "Contact") initializeDomElements(populateContact);
+}
+
 
 console.log("working");
